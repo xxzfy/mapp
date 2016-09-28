3fa565178280472b1f8937fa8ced692342837139
zhyp <xxzfy@163.com>
2016-09-28 10:59:15 +0800

    添加九宫格

---
+++ mapp/src/com/zhao/mapp/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.zhao.mapp;
+
+import android.os.Bundle;
+
+public class LoginActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+}
